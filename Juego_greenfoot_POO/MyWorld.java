41861bcaacdfbd83d6d8c1d2a63945125c0da07f
Fercho1118 <rue23748@uvg.edu.gt>
2023-07-20 09:04:48 -0600

    Creación de actores
    Se añadió el héroe, meteorito y un fondo al mundo.

--- Juego_greenfoot_POO/MyWorld.java
+++ Juego_greenfoot_POO/MyWorld.java
@@ -10,13 +10,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
 
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+    
+    private superheroe heroe; 
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(997, 662, 1);
+        heroe = new superheroe();
+        addObject(heroe, getWidth()/2, getHeight()-50);
     }
 }
